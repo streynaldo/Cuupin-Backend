1b7077c209508f0fc9b87cac4183e88fcd249164
josephwijayaaa <josephwijaya01@gmail.com>
2025-10-25 20:59:15 +0700

    backend api - vendor
    product, bakery

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\ApiBakeryController;
+use App\Http\Controllers\api\ApiProductController;
 use App\Http\Controllers\XenditPaymentController;
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\PaymentController;
@@ -46,14 +47,14 @@ Route::get('/health', function () {
 //     Route::post('/payment-requests/{pr_id}/refund', [PaymentController::class, 'refund']);
 // });
 
-Route::post('/test/ewallet', [XenditPaymentController::class,'create']);
-Route::get('/test/ewallet/{payment_id}', [XenditPaymentController::class,'checkPaymentStatus'])->name('payments.status');
-Route::post('/test/ewallet/refunds', [XenditPaymentController::class,'paymentRefund'])->name('payments.refund');
+Route::post('/test/ewallet', [XenditPaymentController::class, 'create']);
+Route::get('/test/ewallet/{payment_id}', [XenditPaymentController::class, 'checkPaymentStatus'])->name('payments.status');
+Route::post('/test/ewallet/refunds', [XenditPaymentController::class, 'paymentRefund'])->name('payments.refund');
 Route::get('/test/balance', [XenditPaymentController::class, 'checkBalance'])->name('payments.balance');
 Route::post('/test/payouts', [XenditPaymentController::class, 'payout'])->name('payments.payout');
 
-Route::get('/payments/success/{id}', [XenditPaymentController::class,'success'])->name('payments.success');
-Route::get('/payments/failed/{id}',  [XenditPaymentController::class,'failed'])->name('payments.failed');
+Route::get('/payments/success/{id}', [XenditPaymentController::class, 'success'])->name('payments.success');
+Route::get('/payments/failed/{id}',  [XenditPaymentController::class, 'failed'])->name('payments.failed');
 
 Route::prefix('/xendit')->group(function () {
     Route::post('/webhook', [XenditWebhookController::class, 'handle'])->name('xendit.webhook');
@@ -68,11 +69,20 @@ Route::prefix('v1')->group(function () {
     Route::get('/bakeries', [ApiBakeryController::class, 'index']);
     Route::get('/bakeries/{bakery}', [ApiBakeryController::class, 'show']);
 
+    Route::get('/products', [ApiProductController::class, 'index']);
+    Route::get('/products/{product}', [ApiProductController::class, 'show']);
+
     // write: login + ability
-    Route::middleware(['auth:sanctum','abilities:bakeries:write'])->group(function () {
+    Route::middleware(['auth:sanctum', 'abilities:bakeries:write'])->group(function () {
         Route::post('/bakeries', [ApiBakeryController::class, 'store']);
         Route::put('/bakeries/{bakery}', [ApiBakeryController::class, 'update']);
         Route::delete('/bakeries/{bakery}', [ApiBakeryController::class, 'destroy']);
+    });
+
+    Route::middleware(['auth:sanctum', 'abilities:products:write'])->group(function () {
+        Route::post('/products', [ApiProductController::class, 'store']);
+        Route::put('/products/{id}', [ApiProductController::class, 'update']);
+        Route::delete('/products/{id}', [ApiProductController::class, 'destroy']);
     });
 
     // other protected
